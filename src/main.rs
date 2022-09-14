dff558c7a54140da36bf24e94d975d48109fb834
Aadniz <kristianaa25@protonmail.com>
2022-09-14 11:33:38 +0200

    We now have a working system for matching arguments

--- src/main.rs
+++ src/main.rs
@@ -3,36 +3,44 @@ use std::path::Path;
 
 pub mod server; use server::Server;
 
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    /// Possible default servers
-    /// First priority from top to bottom
+    // Possible default servers
+    // First priority from top to bottom
     let mut servers : Vec<Server> = vec![
-        // "http://127.0.0.1:9200",
-        // "http://192.168.1.137:9200"
+        Server::new("http://127.0.0.1:9200/logger"),
+        Server::new("http://192.168.1.137:9200/logger")
     ];
 
-    let mut server = Server::new();
-    servers.push(server);
-
-    /// Possible default locations
-    /// First priority from top to bottom
+    // Possible default locations
+    // First priority from top to bottom
     let mut locations : Vec<&str> = vec![
         "/var/log/nginx/access.log"
     ];
 
     // Iterate arguments, skip executable
     let mut new_locations: Vec<&str> = vec![];
+    let mut new_servers: Vec<Server> = vec![];
     for arg in &args[1..]{
         if Path::new(arg).exists() {
             new_locations.push(arg);
         }
+        else if server::is_url(arg){
+            new_servers.push(Server::new(arg));
+        }
     }
+
     new_locations.reverse();
     locations.reverse();
     locations.extend(new_locations);
     locations.reverse();
+
+    new_servers.reverse();
+    servers.reverse();
+    servers.extend(new_servers);
+    servers.reverse();
 
     println!("Servers:");
     for server in servers{
--- src/server.rs
+++ src/server.rs
@@ -1,37 +1,33 @@
 use std::fmt;
 use regex::Regex;
 
-fn is_url(str : &str) -> bool{
-    let re = Regex::new(r"(http|https)://([^/ :]+):?([^/ ]*)(/?[^ #?]*)\x3f?([^ #]*)#?([^ ]*)").unwrap();
-    if re.is_match(str) && true == true {
-        return re.is_match(str);
-    }
-    return false;
+pub fn is_url(str : &str) -> bool{
+    let re = Regex::new(r#"(http|https)://([^/ :]+):?([^/ ]*)/?(/?[^ #?]*)\x3f?([^ #]*)#?([^ ]*)"#).unwrap();
+    return re.is_match(str);
 }
 
 // This structure is what they call a "class"
-pub(crate) struct Server<'a>{
-    protocol : &'a str,
-    hostname : &'a str,
+pub struct Server{
+    protocol : String,
+    hostname : String,
     port : u16,
-    r#type: &'a str,
-    db : &'a str
+    db : String
 }
-impl<'a> Server<'a>{
-    pub(crate) fn new() -> Self {
+impl Server{
+    pub fn new(str : &str) -> Self {
+        let re = Regex::new(r#"(http|https)://([^/ :]+):?([^/ ]*)/?(/?[^ #?]*)\x3f?([^ #]*)#?([^ ]*)"#).unwrap();
+        let cap = re.captures(str).expect("Expected valid url");
+
         Server {
-            protocol: "test",
-            hostname: "wow",
-            port: 0,
-            r#type: "ok",
-            db: "yap"
+            protocol: String::from(&cap[1]),
+            hostname: String::from(&cap[2]),
+            port: cap[3].parse::<u16>().unwrap_or(9200),
+            db: String::from(&cap[4])
         }
     }
 }
-
-///
-impl fmt::Display for Server<'_>{
+impl fmt::Display for Server{
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}://{}:{}/{}/{}", self.protocol, self.hostname, self.port, self.r#type, self.db)
+        write!(f, "{}://{}:{}/{}", self.protocol, self.hostname, self.port, self.db)
     }
 }
